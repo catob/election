e58dfbac03d09a405256a8684c000ec37989952a
cato b <cato.benjaminsen@gmail.com>
2021-10-26 20:51:58 +0200

    Change contract to include candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,25 @@
 pragma solidity 0.5.16;
 
 contract Election {
-  string public candidate;
+  // Model a candidate
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+  }
+
+  // r/w candidate
+  mapping(uint => Candidate) public candidates;
+
+  uint public candidatesCount;
 
   constructor () public {
-    candidate = "cato";
+    addCandidate('cato');
+    addCandidate('maja');
+  }
+
+  function addCandidate (string memory _name) private {
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
   }
 }
